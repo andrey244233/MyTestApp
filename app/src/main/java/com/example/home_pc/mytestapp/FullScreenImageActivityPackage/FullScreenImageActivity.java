80f6db78ca7ca11778af2439235f5a932b96c642
HOME_PC <a.kushch@andersenlab.com>
2017-11-30 18:10:32 +0200

    commit with swipe

--- app/src/main/java/com/example/home_pc/mytestapp/FullScreenImageActivityPackage/FullScreenImageActivity.java
+++ app/src/main/java/com/example/home_pc/mytestapp/FullScreenImageActivityPackage/FullScreenImageActivity.java
@@ -3,12 +3,12 @@ package com.example.home_pc.mytestapp.FullScreenImageActivityPackage;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.MotionEvent;
 
 import com.example.home_pc.mytestapp.Picture;
 import com.example.home_pc.mytestapp.R;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.github.pwittchen.swipe.library.Swipe;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -16,48 +16,48 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class FullScreenImageActivity extends AppCompatActivity implements View.OnClickListener {
+public class FullScreenImageActivity extends AppCompatActivity {
 
     @BindView(R.id.imageForPicture)
     PhotoView imageViewForPicture;
-    @BindView(R.id.btn_before)
-    ImageButton btnBefore;
-    @BindView(R.id.btn_next)
-    ImageButton btnNext;
     private ArrayList<Picture> pictures = new ArrayList<>();
     private int position;
     private int screenWidth;
     private int screenHeight;
     private FullScreenImageActivityPresenter presenter;
-    private int[] screenParams = new int[2];
-    Uri uri;
+    private Uri uri;
+    private Swipe swipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image);
         ButterKnife.bind(this);
-        btnBefore.setOnClickListener(this);
-        btnNext.setOnClickListener(this);
+
         position = getIntent().getIntExtra(FullScreenImageActivityPresenter.POSITION, 0);
         pictures = (ArrayList<Picture>) getIntent().getSerializableExtra(FullScreenImageActivityPresenter.PICTURES);
         setScreenSize();
         changeImage(position);
+        swipe = new Swipe(300, 300);
+        swipe.setListener(swipeListener);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_before:
-                position -= 1;
-                changeImage(position);
-                break;
-            case R.id.btn_next:
-                position += 1;
-                changeImage(position);
-                break;
+
+    mySwipeListener swipeListener = new mySwipeListener() {
+        @Override
+        public void onSwipedLeft(MotionEvent event) {
+            super.onSwipedLeft(event);
+            position += 1;
+            changeImage(position);
         }
-    }
+
+        @Override
+        public void onSwipedRight(MotionEvent event) {
+            super.onSwipedRight(event);
+            position -= 1;
+            changeImage(position);
+        }
+    };
 
     private void changeImage(int position) {
         if (position < 0) {
@@ -81,6 +81,12 @@ public class FullScreenImageActivity extends AppCompatActivity implements View.O
         int[] screenParams = presenter.getScreenSize(this);
         screenWidth = screenParams[0];
         screenHeight = screenParams[1];
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        swipe.dispatchTouchEvent(event);
+        return super.dispatchTouchEvent(event);
     }
 }
 
